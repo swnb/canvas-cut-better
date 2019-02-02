65b7b3ce1adaa6d8fed92be55c2605bde28d489b
swnb <1786718956@qq.com>
2019-02-03 01:10:03 +0800

    set readonly type

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
 declare module '*.css'
 
-declare type Pos = [number, number];
-declare type Vector = [number, number];
+declare type Pos = Readonly<[number, number]>;
+declare type Vector = Readonly<[number, number]>;
 declare type Paths = Pos[];
