839464c1dcf03c3447e712d26998ea385d699389
swnb <1786718956@qq.com>
2019-02-08 19:35:29 +0800

    init side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import './App.css';
 import { CanvasCutComponent } from 'canvas-cut';
+import { Menu } from './menu';
 import { ErrorBoundary } from 'error-boundary';
 
 class App extends React.Component {
 	public render = () => (
 		<ErrorBoundary>
-			<CanvasCutComponent />;
+			<CanvasCutComponent />
+			<Menu />
 		</ErrorBoundary>
 	);
 }
---
+++ src/menu/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as Styles from './style.module.css';
+
+const { useState } = React;
+
+function useShow(initState: boolean): [boolean, () => void] {
+	const [isShow, setIsShow] = useState(initState);
+
+	return [
+		isShow,
+		() => {
+			setIsShow(!isShow);
+		}
+	];
+}
+
+export const Menu = () => {
+	const [isShow, toggleShow] = useShow(false);
+
+	return (
+		<div
+			className={Styles.welt}
+			style={{
+				right: isShow ? '0' : ''
+			}}
+		>
+			<div className={Styles.button} onClick={toggleShow}>
+				<div className={isShow ? Styles.iconShow : Styles.iconHide} />
+			</div>
+			<div className={Styles.menu} />
+		</div>
+	);
+};
